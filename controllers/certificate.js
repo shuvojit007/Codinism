a28e9441ea2baee475feef6ed56475ac61e2bfad
shuvojit007 <shuvojitkar241@gmail.com>
2018-09-23 23:39:03 +0600

    New Commit'

--- controllers/certificate.js
+++ controllers/certificate.js
@@ -8,9 +8,9 @@ module.exports={
      try {
         var loadedImage = await jimp.read(fileName);
         var font = await jimp.loadFont(jimp.FONT_SANS_128_BLACK);
-         var Image =  await loadedImage.print(font, loadedImage.bitmap.width-1150,1300,req.body.name).resize(800, jimp.AUTO).write();
-        var result = await imgur.uploadFile(Image.bitmap)
-        res.status(200).json(result.data.link)
+        await loadedImage.print(font, loadedImage.bitmap.width-1000,1300,req.body.name).resize(800, jimp.AUTO).write('output.png');
+        var result = await imgur.uploadFile("output.png")
+        res.status(200).send(result.data.link)
     } catch(e) {
         res.status(500).json({
             message: 'Something Went Wrong',
