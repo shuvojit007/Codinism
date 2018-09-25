77474280c81859eb78a393a95ea94927b36ed87d
shuvojit007 <shuvojitkar241@gmail.com>
2018-09-25 23:59:19 +0600

    new commit

--- controllers/certificate.js
+++ controllers/certificate.js
@@ -15,8 +15,11 @@ module.exports={
         var font = await jimp.loadFont(jimp.FONT_SANS_128_BLACK);
         await loadedImage.print(font, loadedImage.bitmap.width-1100,1300,req.body.name).resize(800, jimp.AUTO).write(imagename+".png");
         var result = await imgur.uploadFile(imagename+".png")
-        res.status(200).send(result.data.link)
+        res.status(200).send({data:result.data.link})
+        console.log(result)
         await fs.unlink(imagename+".png");
+
+    
         
     } catch(e) {
         res.status(500).json({
