04e5afa5fdc150a9b5529dd62f62e0c5add76a83
shuvojit007 <shuvojitkar241@gmail.com>
2018-09-26 18:49:34 +0600

    New Commit

--- controllers/SendMailtoUser.js
+++ controllers/SendMailtoUser.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 const config = require('../config/config');
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: config.nodemailer.email,
       pass: config.nodemailer.pass
     }
 });
+
 
 module.exports ={
     SendMailtoUser :async (req,res)=>{
