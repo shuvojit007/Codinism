15d5a5f0308fbee3b9bd842da1f2541aa8fbd4d5
shuvojit007 <shuvojitkar241@gmail.com>
2018-09-27 13:21:16 +0600

    New Commit

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     imgur: '81c9941fb465a67',
     nodemailer :{
-        email:"thejavalover007@gmail.com",
-        pass:"shuvojit26044"
+        email:"programmingheroteam@gmail.com",
+        pass:"codinism"
     }
 };
